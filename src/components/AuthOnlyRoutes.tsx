8963245fd7a261b02bddddb61c988b357f675822
AlexReznichenko <alex.r@itmaster-soft.com>
2023-03-02 17:37:44 +0300

    Add main layout with navigation menu.

--- src/components/AuthOnlyRoutes.tsx
+++ src/components/AuthOnlyRoutes.tsx
@@ -1,21 +1,17 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "src/app/hooks";
-import { logOut, selectToken } from "src/app/features/user/userSlice";
-import { Box, Button } from "@mui/material";
+import { useAppSelector } from "src/app/hooks";
+import { selectToken } from "src/app/features/user/userSlice";
+import { Box } from "@mui/material";
+import { MainLayout } from "src/components/MainLayout/MainLayout";
 
 const AuthOnlyRoutes: React.FC = () => {
   const token = useAppSelector(selectToken);
-  const dispatch = useAppDispatch();
   return token ? (
-    <Box>
-      <Box sx={{ m: 3 }}>
-        <Button variant={"contained"} onClick={() => dispatch(logOut())}>
-          Выход
-        </Button>
-      </Box>
+    <MainLayout>
+      <Box sx={{ m: 3 }}></Box>
       <Outlet />
-    </Box>
+    </MainLayout>
   ) : (
     <Navigate to="/login" />
   );
